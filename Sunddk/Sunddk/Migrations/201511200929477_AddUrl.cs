bc323e95e2d2670ad05492f8421971fdeefad59b
NicolajPedersen <nicolajgpedersen@gmail.com>
2015-11-20 10:53:30 +0100

    Måske

---
+++ Sunddk/Sunddk/Migrations/201511200929477_AddUrl.cs
@@ -0,0 +1,16 @@
+namespace Sunddk.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUrl : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
