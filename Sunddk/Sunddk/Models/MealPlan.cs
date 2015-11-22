f7d5815b2be2ea4e77c0b5c7d32c70cd91542049
NicolajPedersen <nicolajgpedersen@gmail.com>
2015-11-22 17:30:15 +0100

    Jenes commit fik Db til at virke!! :)

--- Sunddk/Sunddk/Models/MealPlan.cs
+++ Sunddk/Sunddk/Models/MealPlan.cs
@@ -12,6 +12,6 @@ namespace Sunddk.Models
         public double MaxCalories { get; set; }
         public string Description { get; set; }
         public virtual List<Meal> Meals { get; set; }
-        public  List<int> MealsId { get; set; }
+        public List<int> MealsId { get; set; }
     }
 }
