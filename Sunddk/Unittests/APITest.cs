d7a5c90e1238a409b1b3dbe8499dff530d2cb909
jensskjold <jmskjold@gmail.com>
2015-11-26 09:23:19 +0100

    Object object type object

--- Sunddk/Unittests/APITest.cs
+++ Sunddk/Unittests/APITest.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SunddkAPI;
+using SunddkAPI.Models;
 
 namespace Unittests
 {
     [TestClass]
     public class APITest
     {
+        // Integration test
         [TestMethod]
         public void TestCreatePerson()
         {
-            SunddkAPI.Controllers.PersonController controller = new SunddkAPI.Controllers.PersonController();
-            bool isCreated = controller.CreateUser("Testo", Convert.ToDateTime("2001-01-01"), false, "Mand", "test@live.dk", "Test123", DateTime.Now, 90, 175, 1000);
+            SunddkAPI.Controllers.PersonController controller = new SunddkAPI.Controllers.PersonController(); // nyt objekt
+            Person p = new Person() { Name = "Testo", DateOfBirth = Convert.ToDateTime("2001-01-01"), IsAdmin = false, Gender = "Mand", Email = "test@live.dk", Password = "Test123" };
+            p.Measurements = new System.Collections.Generic.List<Measurement>();
+            p.Measurements.Add(new Measurement() { Date = DateTime.Now, Weight = 90, Height = 175, BMR = 1000 });
+            bool isCreated = controller.CreateUser(p);
             Assert.AreEqual(true, isCreated);
             SunddkAPI.Database.DataMapper.DeleteUser("test@live.dk");
 
